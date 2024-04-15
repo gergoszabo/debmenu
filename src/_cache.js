b83143374456cf50d639f3d29b0bf39552b9c094
Gergo Szabo <gergo.szabo@epam.com>
2024-04-15 10:00:16 +0200

    add forest restaurant

--- src/_cache.js
+++ src/_cache.js
@@ -1,10 +1,11 @@
-import { existsSync, mkdirSync, readFileSync, rmSync, statSync, writeFileSync } from 'node:fs';
+import { existsSync, mkdirSync, writeFileSync } from 'node:fs';
+// import { existsSync, mkdirSync, readFileSync, rmSync, statSync, writeFileSync } from 'node:fs';
 
 export const RESULT_FOLDER = 'result';
 export const CACHE_FOLDER = 'cache';
-const CACHE_INTERVAL = 1 * 60 * 60 * 1000; // 1 hour
+// const CACHE_INTERVAL = 1 * 60 * 60 * 1000; // 1 hour
 
-const noCache = process.argv.includes('--no-cache');
+// const noCache = process.argv.includes('--no-cache');
 
 export function createCacheFolder() {
     return createFolder(CACHE_FOLDER);
@@ -23,48 +24,51 @@ function createFolder(folder) {
 export async function cacheOrFetch(filename, fetchUrl, type) {
     const cacheFileName = `${CACHE_FOLDER}/${filename}.cache.${type}`;
     let result;
-    if (
-        !noCache &&
-        existsSync(cacheFileName) &&
-        statSync(cacheFileName).mtimeMs > (Date.now() - CACHE_INTERVAL)
-    ) {
-        console.log(`READ ${cacheFileName}`);
-        switch (type) {
-            case 'json':
-                result = JSON.parse(readFileSync(cacheFileName, { encoding: 'utf8' }));
-                break;
-            case 'html':
-                result = readFileSync(cacheFileName, { encoding: 'utf8' });
-                break;
-            case 'png':
-                result = readFileSync(cacheFileName, { encoding: null });
-                break;
-            default:
-                throw new Error(`Not implemented case! ${type}`);
-        }
-        return result;
-    } else {
-        if (existsSync(cacheFileName)) {
-            rmSync(cacheFileName);
-        }
-        console.log(`GET ${fetchUrl}`);
-        switch (type) {
-            case 'json':
-                result = await (await fetch(fetchUrl)).json();
-                writeFileSync(cacheFileName, JSON.stringify(result, null, 4));
-                break;
-            case 'html':
-                result = await (await fetch(fetchUrl)).text();
-                writeFileSync(cacheFileName, result);
-                break;
-            case 'png':
-                result = await (await fetch(fetchUrl)).arrayBuffer();
-                result = Buffer.from(result);
-                writeFileSync(cacheFileName, result);
-                break;
-            default:
-                throw new Error(`Not implemented case! ${type}`);
-        }
-        return result;
+    // temporarly disable cache
+    // if (
+    //     !noCache &&
+    //     existsSync(cacheFileName) &&
+    //     statSync(cacheFileName).mtimeMs > (Date.now() - CACHE_INTERVAL)
+    // ) {
+    //     console.log(`READ ${cacheFileName}`);
+    //     switch (type) {
+    //         case 'json':
+    //             result = JSON.parse(readFileSync(cacheFileName, { encoding: 'utf8' }));
+    //             break;
+    //         case 'html':
+    //             result = readFileSync(cacheFileName, { encoding: 'utf8' });
+    //             break;
+    //         case 'jpg':
+    //         case 'png':
+    //             result = readFileSync(cacheFileName, { encoding: null });
+    //             break;
+    //         default:
+    //             throw new Error(`Not implemented case! ${type}`);
+    //     }
+    //     return result;
+    // } else {
+    //     if (existsSync(cacheFileName)) {
+    //         rmSync(cacheFileName);
+    //     }
+    console.log(`GET ${fetchUrl}`);
+    switch (type) {
+        case 'json':
+            result = await (await fetch(fetchUrl)).json();
+            writeFileSync(cacheFileName, JSON.stringify(result, null, 4));
+            break;
+        case 'html':
+            result = await (await fetch(fetchUrl)).text();
+            writeFileSync(cacheFileName, result);
+            break;
+        case 'jpg':
+        case 'png':
+            result = await (await fetch(fetchUrl)).arrayBuffer();
+            result = Buffer.from(result);
+            writeFileSync(cacheFileName, result);
+            break;
+        default:
+            throw new Error(`Not implemented case! ${type}`);
     }
+    return result;
+    // }
 }
---
+++ src/forest.js
@@ -0,0 +1,172 @@
+import * as cheerio from 'cheerio';
+import { CACHE_FOLDER, cacheOrFetch, RESULT_FOLDER } from './_cache.js';
+import { getDateRange } from './_date.js';
+import { detectText } from './services/aws.js';
+import { spawnSync } from 'node:child_process';
+
+const fetchUrl = 'https://forestetterem.hu/';
+const website = 'https://forestetterem.hu';
+const shortName = 'forest';
+const name = 'Forest étterem';
+
+const startedAt = performance.now();
+export const fetchForest = async () => {
+    try {
+        // fetch page or load it from cache
+        const html = await cacheOrFetch(shortName, fetchUrl, 'html');
+
+        // parse out image src
+        const $ = cheerio.load(html);
+        const imgSrc = $('.et_pb_lightbox_image').attr('href');
+
+        // fetch and cache the image
+        await cacheOrFetch(shortName, imgSrc, 'jpg');
+
+        const offers = [];
+        // adjust huse image to make the background image and text disappear
+
+        // brew install imagemagick
+        // magick huse.cache.png -evaluate Add 10% huse.magicked.png
+        const adjustedImageFileName = `${RESULT_FOLDER}/${shortName}.magick.jpg`;
+        spawnSync('convert', [
+            `${CACHE_FOLDER}/${shortName}.cache.jpg`,
+            '-evaluate',
+            'Add',
+            '-20%',
+            adjustedImageFileName,
+        ], {
+            encoding: 'utf8',
+            stdio: 'inherit',
+            cwd: process.cwd(),
+        });
+
+        const cropFileNames = createCropImages();
+
+        const weekFileName = cropFileNames.shift();
+
+        const weekDetectResponse = await detectText(weekFileName);
+        const weekStrLine = weekDetectResponse?.TextDetections.find((td) => td.Type === 'LINE');
+        console.log(weekStrLine?.DetectedText);
+        // НЕТІ MENÜ ÁPRILIS 15 - 19.
+        const week = weekStrLine.DetectedText.split(' ').slice(2).filter((s) => !!s).join(' ');
+        const dates = parseDatesFromDateRangeLine(week);
+
+        let dateIndex = 0;
+        const threshold = 0.05;
+        for (const filename of cropFileNames) {
+            console.log(`Processing ${filename}`);
+            const response = await detectText(filename);
+
+            let previousBoxTop = -1;
+            const offersForTheDay = [];
+            const lines = (response.TextDetections || [])?.filter((td) => td.Type === 'LINE');
+
+            for (let index = 1; index < lines.length; index++) {
+                const line = lines[index];
+                if (
+                    (line?.Geometry?.BoundingBox?.Top || 0) - previousBoxTop < threshold
+                ) {
+                    offersForTheDay[offersForTheDay.length - 1] += line?.DetectedText.toLowerCase();
+                } else {
+                    offersForTheDay.push((line?.DetectedText || '').toLowerCase());
+                }
+                previousBoxTop = line.Geometry?.BoundingBox?.Top || previousBoxTop;
+            }
+
+            offers.push({
+                date: dates[dateIndex++].toISOString().substring(0, 10).replaceAll('-', '.'),
+                day: 'text',
+                offers: offersForTheDay.filter((o) => o !== 'levesek' && o !== 'föételek'),
+            });
+        }
+
+        return {
+            shortName,
+            fetchedIn: performance.now() - startedAt,
+            fetchUrl,
+            website,
+            name,
+            offers,
+        };
+    } catch (e) {
+        return {
+            shortName,
+            fetchedIn: performance.now() - startedAt,
+            name,
+            errorReason: e.message + '\n' + e.stack,
+            fetchUrl,
+            offers: [],
+            website,
+        };
+    }
+};
+
+const MONTH_DICT = {
+    'január': '01',
+    'február': '02',
+    'március': '03',
+    'április': '04',
+    'május': '05',
+    'június': '06',
+    'július': '07',
+    'augusztus': '08',
+    'szeptember': '09',
+    'október': '10',
+    'november': '11',
+    'december': '12',
+};
+
+function parseDatesFromDateRangeLine(dateRangeLine) {
+    // ÁPRILIS 15 - 19.
+    const x = dateRangeLine
+        .toLowerCase()
+        .trim()
+        .replaceAll('.', '')
+        .replaceAll('-', '')
+        .split(' ')
+        .filter((s) => !!s);
+    // ['április', '15', '19']
+
+    const startDate = new Date().getFullYear().toString() + '-' +
+        MONTH_DICT[x[0]] + '-' +
+        x[1];
+
+    const endDate = new Date().getFullYear().toString() + '-' +
+        MONTH_DICT[x[0]] + '-' +
+        x[2];
+
+    return getDateRange(startDate, endDate);
+}
+
+function createCropImages() {
+    // 800x573
+    const cropWidth = 147;
+    const cropStartHeight = 66;
+    const cropHeight = 410 - cropStartHeight;
+    const crops = [
+        [0, 0, 800, cropStartHeight],
+        [30, cropStartHeight, cropWidth, cropHeight - cropStartHeight / 2],
+        [30 + cropWidth, cropStartHeight, cropWidth, cropHeight - cropStartHeight / 2],
+        [30 + cropWidth * 2, cropStartHeight, cropWidth, cropHeight - cropStartHeight / 2],
+        [30 + cropWidth * 3, cropStartHeight, cropWidth, cropHeight],
+        [30 + cropWidth * 4, cropStartHeight, cropWidth, cropHeight],
+    ];
+    const cropFileNames = [];
+    for (let i = 0; i < 6; i++) {
+        const { cropFileName, cmd } = getImageCropCommand(crops[i], i + 1);
+        cropFileNames.push(cropFileName);
+        spawnSync(cmd[0], cmd.slice(1));
+    }
+    return cropFileNames;
+}
+
+function getImageCropCommand(bounds, index) {
+    const cropFileName = `${RESULT_FOLDER}/${shortName}-${index}-crop.jpg`;
+    const cropParams = `${bounds[2]}x${bounds[3]}+${bounds[0]}+${bounds[1]}!`;
+
+    return {
+        cmd: `convert ${RESULT_FOLDER}/${shortName}.magick.jpg -crop ${cropParams} ${cropFileName}`
+            .split(' '),
+        cropFileName,
+    };
+}
