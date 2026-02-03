68c165638bdbfa7341f97590af45f75d093e90ab
Gergő Szabó <garyundead@gmail.com>
2026-02-03 08:58:43 +0100

    adjust email template

--- debmenu_run.sh
+++ debmenu_run.sh
@@ -3,4 +3,4 @@
 # Log datetime before running script
 date '+%Y-%m-%d %H:%M:%S Running debmenu_run.sh' >> /apps/debmenu/log.log
 # Execute the debmenu script and append output to log.log
-node /apps/debmenu/debmenu.cjs >> /app/debmenu/log.log 2>&1
+node /apps/debmenu/debmenu.cjs >> /apps/debmenu/log.log 2>&1
