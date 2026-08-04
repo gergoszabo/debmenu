9552c226e5ed48c8104ce63401670e5dc2c5ab2b
Szabó Gergő <316282+gergoszabo@users.noreply.github.com>
2026-08-04 22:28:24 +0200

    Adjust cron + run (#70)

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-#docker run --name debmenu --rm -d -v $(pwd):/app:rw -v app-cache:/cache -v ~/.microsoft/user-secrets:/root/.microsoft/user-secrets:ro mcr.microsoft.com/dotnet/sdk:10.0 bash -c "cd /app && dotnet run"
-#dotnet run
+dotnet build
+dotnet publish
 cd ./bin/Release/net10.0/linux-x64/publish && ./debmenu
